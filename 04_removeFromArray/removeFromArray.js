4f02475098cd6b466609ae28e3b42ae7744b8916
NievaS24 <santinieva24@gmail.com>
2025-01-26 22:48:14 -0300

    fix: :white_check_mark: Funcion removeFromArray creada
    Funcion creada para pasar los tests. La solucion de TOP es diferente.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    if (args.length > 0) {
+        for (let i = 0; i < args.length; i++) {
+            do {
+                index = array.indexOf(args[i]);
+                if (index != -1) {
+                    array.splice(index,1);
+                }           
+            } while (index != -1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
