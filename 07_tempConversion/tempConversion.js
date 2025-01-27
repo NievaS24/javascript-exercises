a93bb374d1d5aea8dbc7e6126edb5a7b8bb23955
NievaS24 <santinieva24@gmail.com>
2025-01-26 23:26:40 -0300

    fix: :white_check_mark: Funciones convertToCelsius y convertToFahrenheit creadas.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * 5 / 9;
+  if (Number.isInteger(tempC)){
+    return tempC;
+  } else {
+    return parseFloat(tempC.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * 9 / 5) + 32
+  if (Number.isInteger(tempF)){
+    return tempF;
+  } else {
+    return parseFloat(tempF.toFixed(1));
+  }
 };
 
 // Do not edit below this line
