cbe9db9c964fcd68f9d72610591b4afd83f140b3
NievaS24 <santinieva24@gmail.com>
2025-01-26 21:01:54 -0300

    fix: :white_check_mark: Funcion reverseString creada
    Funcion para los tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    let length = string.length;
+    for (let i = 1 ; i <= length ; i++) {
+        newString += string[length - i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
