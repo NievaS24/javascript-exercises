f12ad779f85be8be733e0c5d5e22e0fc8226109e
NievaS24 <santinieva24@gmail.com>
2025-01-31 17:14:11 -0300

    Ejercicio resuelto con ayuda de la solucion.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    string = string
+                        .toLowerCase()
+                        .split("")
+                        .filter(item => alphanumerical.includes(item))
+                        .join("");
+
+    const reverseString =  string
+                                .split("")
+                                .reverse()
+                                .join("");
+    return string == reverseString;
 };
 
 // Do not edit below this line
