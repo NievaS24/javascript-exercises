f5038a9ca12655c785e6855a9555aeccae15df11
NievaS24 <santinieva24@gmail.com>
2025-01-31 16:13:24 -0300

    Ejercicio realizado.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    const sum = array.reduce((total, current) => total + current, 0);
+    return sum;
+  };
+  
+  const multiply = function(array) {
+    const product = array.reduce((total, current) => total * current, 1);
+    return product;  
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+    let result = 1;
+    for (let i = 0; i < b; i++) {
+      result *= a;
+    }
+    return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+      let result = 1;
+      for (let i = a; i > 0 ; i--) {
+        result *= i;
+      }
+      return result;
 };
 
 // Do not edit below this line
