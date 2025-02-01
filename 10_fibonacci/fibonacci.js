cb59f21b113fe87e6607d1e476b27b9991eb3d8f
NievaS24 <santinieva24@gmail.com>
2025-02-01 20:01:42 -0300

    Ejercicio resuelto.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number)
+    if (number < 0) {
+        return "OOPS";
+    } else if (number == 1) {
+        return 1;
+    }
+    let currentNum = 1;
+    let prevNum = 0;
+    let result = 0;
+    for (let i = 1; i < number; i++) {
+        result = currentNum + prevNum;  // 1 - 2 - 3
+        prevNum = currentNum; // 1 - 1 -2
+        currentNum = result; // 1 - 2 - 3
+    }
+    return result;
 };
 
 // Do not edit below this line
