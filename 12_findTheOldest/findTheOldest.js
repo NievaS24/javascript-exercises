18bbe93eb95fff23a70f80f7006f5119e0774caf
NievaS24 <santinieva24@gmail.com>
2025-02-01 20:30:42 -0300

    Ejercicio resuelto.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        let aAge, bAge;
+        if (a.yearOfDeath != undefined) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            aAge = 2025 - a.yearOfBirth;
+        }
+        if (b.yearOfDeath != undefined) {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            bAge = 2025 - b.yearOfBirth;
+        }
+        return aAge - bAge;
+    });
+    return people[people.length -1];
 };
 
 // Do not edit below this line
