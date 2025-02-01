76fa4677f07b34845ec3fb51a761542b265981b3
NievaS24 <santinieva24@gmail.com>
2025-02-01 20:40:44 -0300

    Ejercicio mejorado.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,13 @@
+const getAge = function (birth, death) {
+    if (!death){
+         death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
 const findTheOldest = function(people) {
-    people.sort((a, b) => {
-        let aAge, bAge;
-        if (a.yearOfDeath != undefined) {
-            aAge = a.yearOfDeath - a.yearOfBirth;
-        } else {
-            aAge = 2025 - a.yearOfBirth;
-        }
-        if (b.yearOfDeath != undefined) {
-            bAge = b.yearOfDeath - b.yearOfBirth;
-        } else {
-            bAge = 2025 - b.yearOfBirth;
-        }
-        return aAge - bAge;
-    });
-    return people[people.length -1];
+    people.sort((prevPerson, currPerson) =>  getAge(prevPerson.yearOfBirth, prevPerson.yearOfDeath) - getAge (currPerson.yearOfBirth, currPerson.yearOfDeath));
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
